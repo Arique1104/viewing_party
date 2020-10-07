68ec344f0c8fd3a770369e8e555023b2454054ff
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-07 15:56:59 -0600

    SUBJECT: Add friendship model and update user model
    DETAILS: Add follower/followee relationships nested under class:"User" to frienship model; Update user model with self-referntial relationships to followers and followees
    
    Co-authored-by: Arique <60521756+Arique1104@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-  has_secure_password 
 
+  has_secure_password
+
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Friendship'
+  has_many :followees, through: :followed_users
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Friendship'
+  has_many :followers, through: :following_users
+
+  # have_many :parties
+  # have_many :party_participants, through: :parties
 end
