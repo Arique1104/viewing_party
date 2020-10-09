b886fa12742cf9a593f75f771e489a2caccdfce2
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-09 12:02:58 -0600

    SUBJECT: Add Party database and joins for party_participants
    DETAILS:
    - Party Database: has a user foreign key; has strings for movie_id, runtime, date, and start
    - Party_Participants: a joins table that pulls friendships:references and parties:references (which will be nested under a user_id in party)
    
    Co-authored-by: Arique <60521756+Arique1104@users.noreply.github.com>

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
+  has_many :party_participants
+  has_many :parties, through: :party_participants
 end
