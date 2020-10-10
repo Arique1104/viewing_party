8cdfb98e7a8794a9512f5519bc17f792eb5e87fc
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-10 16:26:41 -0500

    Beginnings of searching for top 40 films in search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,13 @@
 class SearchController < ApplicationController
   def top_forty
+    conn = Faraday.new(url: "https://api.themoviedb.org")
+    # do |faraday|
+    #   # faraday.headers['media_type'] = 'movies'
+    #   # faraday.headers['time_window'] = 'day'
+    #   faraday.headers['api_key'] = ENV["MOVIE_API_KEY"]
+    # end
+
+    response = conn.get("/discover/movie?sort_by=popularity.desc/?#{ENV["MOVIE_API_KEY"]}")
+    # require "pry"; binding.pry
   end
 end
