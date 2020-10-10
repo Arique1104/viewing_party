950463ed00a2f65e7f6f5d023ac77a0e79807562
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-10 10:00:50 -0600

    SUBJECT: Update users show action
    DETAILS: Add a condition to check if email exists in database; If conditions met - send an instance of user to the view; if not, flash error and return to welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def show
+    if User.exists?(email:params[:email])
+      @user = User.find_by(email:params[:email])
+    else
+      flash[:error] = "Your credentials do not match any users in the system. Please register first."
+      redirect_to "/"
+      #Probably want to discuss, should this route back to welcome or to /register?
+    end
   end
 end
