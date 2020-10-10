b429b35597f3714ae437b4eed5229d806cf34e44
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-10 09:27:32 -0600

    SUBJECT: Update Welcome Spec
    DETAILS: Add initial workings of 3 new tests and updates original test to match Issue#15 Welcome Page

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -2,11 +2,35 @@ require "rails_helper"
 
 RSpec.describe "Welcome Page" do
   describe "as a visitor" do
-    it "can show welcome page" do
+    before :each do
+      @rainbow_dash = User.create!(email:"rainbow_dash@email.com", password:"user")
+      @pinkie_pie = User.create!(email:"pinkie_pie@email.com", password:"user")
+      @twilight_sparkle = User.create!(email:"twilight_sparkle@email.com", password:"user")
+      @rarity = User.create!(email:"rarity@email.com", password:"user")
+      @applejack = User.create!(email:"applejack@email.com", password:"user")
+      @fluttershy = User.create!(email:"fluttershy@email.com", password:"user")
+      @spike = User.create!(email:"spike@email.com", password:"user")
+      @starlight_glimmer = User.create!(email:"starlight_glimmer@email.com", password:"user")
 
+      @rainbow_dash.followers << [@rarity, @applejack, @twilight_sparkle]
+      @twilight_sparkle.followers << [@spike, @starlight_glimmer]
+      @pinkie_pie.followers << [@rainbow_dash, @twilight_sparkle, @rarity, @applejack, @fluttershy, @spike, @starlight_glimmer]
+    end
+
+    it "can show welcome message" do
       visit '/'
+      expect(page).to have_content("Welcome to Viewing Party")
+    end
 
-      expect(page).to have_content("Welcome")
+    it "can show a brief description of the application" do
+
+    end
+
+    it "can show email and password fields with a button to login" do
+
+    end
+
+    it "can show a link to register" do
 
     end
   end
