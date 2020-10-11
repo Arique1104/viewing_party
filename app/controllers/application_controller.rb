83fe29b429f590e4a1f1eadbbb9f5520b160c72f
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-11 15:22:12 -0600

    SUBJECT: Add generate flash method to application controller
    DETAILS:
    - Method calls rails .errors on resource
    - Creates a flash error message that is dynamic
    - Errors force email to be an email
    - Errors force password and password_confirmation to match
    - Errors force password to authenticate a registered user upon login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def generate_flash(resource)
+    error_messages = String.new
+    resource.errors.messages.each do |validation, message|
+      error_messages += "#{validation.capitalize} (#{message[0]}); "
+    end
+    error_messages = error_messages.delete_suffix("; ")
+    flash[:failure] = "ERROR: #{error_messages}"
+  end
 end
