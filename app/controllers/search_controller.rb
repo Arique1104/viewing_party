d5051f1d202a1e9f3722bd2d54539cc1c002b7a3
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-11 14:05:09 -0500

    SUBJECT: API key is properly loaded in the environment variable and the call is successful.
    DETAILS: This work was the result of a paired session with Priya as the navigator and Arique as the driver.
    
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   def top_forty
     conn = Faraday.new(url: "https://api.themoviedb.org")
     # do |faraday|
+    # conn = Faraday.new(url: "https://api.themoviedb.org")# do |faraday|
     #   # faraday.headers['media_type'] = 'movies'
     #   # faraday.headers['time_window'] = 'day'
     #   faraday.headers['api_key'] = ENV["MOVIE_API_KEY"]
@@ -9,5 +10,14 @@ class SearchController < ApplicationController
 
     response = conn.get("/discover/movie?sort_by=popularity.desc/?#{ENV["MOVIE_API_KEY"]}")
     # require "pry"; binding.pry
+    top_40 = []
+    2.times do
+      num = 1
+      response = Faraday.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["MOVIE_API_KEY"]}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=#{num}")
+      json = JSON.parse(response.body, symbolize_names: true)
+      top_40 << json[:results]
+      num += 1
+    end
+    @results = top_40.flatten
   end
 end
