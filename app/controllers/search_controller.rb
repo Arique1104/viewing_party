653d29c9a273c0ce781bb9fdf1d0eda4f6e6cd8c
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-11 15:07:54 -0500

    Removes exposing API Key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,7 @@
 class SearchController < ApplicationController
   def top_forty
     conn = Faraday.new(url: "https://api.themoviedb.org")
-    # do |faraday|
-    # conn = Faraday.new(url: "https://api.themoviedb.org")# do |faraday|
-    #   # faraday.headers['media_type'] = 'movies'
-    #   # faraday.headers['time_window'] = 'day'
-    #   faraday.headers['api_key'] = ENV["MOVIE_API_KEY"]
-    # end
-
     response = conn.get("/discover/movie?sort_by=popularity.desc/?#{ENV["MOVIE_API_KEY"]}")
-    # require "pry"; binding.pry
     top_40 = []
     2.times do
       num = 1
