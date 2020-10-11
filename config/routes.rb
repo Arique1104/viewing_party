8125898169ab61ad0b27c0fe4991ce53ff600404
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-11 13:10:51 -0600

    SUBJECT: Add register and post users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/", to: 'welcome#index', as: :welcome
   # resources :welcome, :path => '/', only: :index, as: :welcome
-
+  get "/register", to: "users#new"
+  post "/users", to: "users#create"
 end
