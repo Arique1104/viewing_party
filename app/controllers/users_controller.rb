2feb0b8851441d0a8a9c525b973bb63cf2d8b34a
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-12 13:59:26 -0600

    SUBJECT: Update Users controller
    DETAILS: Remove password_confirmation from users private params permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password)
   end
 end
