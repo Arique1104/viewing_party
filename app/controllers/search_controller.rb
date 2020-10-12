bbc0f4cd86ce4efeaf41aaa52dbca952007a8063
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-12 09:23:55 -0500

    Updates the app controller and removes any comments along with origina api key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,13 +3,16 @@ class SearchController < ApplicationController
     conn = Faraday.new(url: "https://api.themoviedb.org")
     response = conn.get("/discover/movie?sort_by=popularity.desc/?#{ENV["MOVIE_API_KEY"]}")
     top_40 = []
+    num = 1
     2.times do
-      num = 1
       response = Faraday.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["MOVIE_API_KEY"]}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=#{num}")
+      require "pry"; binding.pry
       json = JSON.parse(response.body, symbolize_names: true)
       top_40 << json[:results]
       num += 1
     end
-    @results = top_40.flatten
+    @results = top_40.flatten.sort_by do |result|
+      result[:vote_average]
+    end.reverse
   end
 end
