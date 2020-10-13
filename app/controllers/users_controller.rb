0c6421b8206cff069fc950c8d8caec8f39fc31f9
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-13 15:26:07 -0600

    SUBJECT: Update users controller show and add method add_friend
    DETAILS:
    - Show will instantiate an array/AR-array of friendships from user database
    - Above might need refactor using more AR
    - New method: Add_friend
    - This method takes the users input on dashboard and confirms if it is in database
    - If not, flash error
    - If yes, add friendship in BOTH directions
    - redirect back to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,25 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+
+    @friendships = []
+    if !@user.friendships.empty?
+      @user.friendships.each do |friend_record|
+        id = friend_record.friend_id
+        @friendships << User.find(id)
+      end
+    end
+  end
+
+  def add_friend
+    new_friend = User.find_by(email:params[:email])
+    if new_friend.nil?
+      flash[:error] = "I'm sorry your friend cannot be found"
+    else
+      current_user.friendships.create!(friend:new_friend)
+      new_friend.friendships.create!(friend:current_user)
+    end
+    redirect_to "/dashboard"
   end
 
   private
