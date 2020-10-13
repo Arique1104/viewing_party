ed31542854e21b4890241fdb2271e7b477b832fe
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-12 18:56:12 -0600

    SUBJECT: Update controller files for rubocop violation fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,24 @@
 class UsersController < ApplicationController
   before_action :require_user, only: :show
-  
-  def new
-
-  end
+  def new; end
 
   def create
     new_user = User.new(user_params)
     if new_user.save
       session[:user_id] = new_user.id
-      redirect_to "/dashboard"
+      redirect_to '/dashboard'
     else
       generate_flash(new_user)
-      redirect_to "/register"
+      redirect_to '/register'
     end
   end
 
   def show
-    # @user = User.find(session[:user_id])
     @user = current_user
   end
 
   private
+
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,30 +3,20 @@ class User < ApplicationRecord
   has_many :followees, through: :followed_users
   has_many :following_users, foreign_key: :followee_id, class_name: 'Friendship'
   has_many :followers, through: :following_users
-  has_many :parties
+  has_many :parties, dependent: :destroy
   # have_many :party_participants, through: :parties
 
   has_secure_password
 
-  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } #Must be a valid email
-  validates_confirmation_of :password, require: true
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, confirmation: true
   validate :password_complexity
 
   def password_complexity
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
 
-    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    errors.add  :password,
+                'Complexity requirement not met.
+                Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
-
-  # validates :password,
-  #   allow_nil: true,
-  #   length: { in: Devise.password_length },
-  #   format: { with: PASSWORD_FORMAT },
-  #   confirmation: true,
-  #   on: :update
-
-
-
-
-
 end
