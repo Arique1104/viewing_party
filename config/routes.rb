3ef5793c7163a6a6c65bf9164632b614b8739737
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-12 18:35:27 -0600

    SUBJECT: Add get discover to discover#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/", to: 'welcome#index', as: :welcome
   # resources :welcome, :path => '/', only: :index, as: :welcome
+
+  get '/discover', to: 'discover#show'
   get "/register", to: "users#new"
   post "/users", to: "users#create"
   get "/dashboard", to: "users#show"
