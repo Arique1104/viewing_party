bdd1eb23ef64f93c1c3d93c1536ae1ec55839c93
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-13 13:34:17 -0600

    SUBJECT: Update dashboard spec
    DETAILS:
    - Update dashboard spec for unique emails
    - Add friendship testing including a test for no friends

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -3,33 +3,21 @@ require "rails_helper"
 RSpec.describe "Dashboard Page", type: :feature do
   describe "As a registered user" do
     before :each do
-      @twilight_sparkle = User.create!(email:"twilight_sparkle@email.com", password:"User@us3r")
+      @twilight_sparkle = User.create!(name:"Twilight", email:"twilight_sparkle@email.com", password:"User@us3r")
 
-      @spike = User.create!(email:"spike@email.com", password:"User@us3r")
-      @starlight_glimmer = User.create!(email:"starlight_glimmer@email.com", password:"User@us3r")
-      @pinkie_pie = User.create!(email:"pinkie_pie@email.com", password:"User@us3r")
+      @spike = User.create!(name:"spike", email:"spike@email.com", password:"User@us3r")
+      @starlight_glimmer = User.create!(name:"Starlight", email:"starlight_glimmer@email.com", password:"User@us3r")
+      @pinkie_pie = User.create!(name:"pinkie", email:"pinkie_pie@email.com", password:"User@us3r")
 
-      @rainbow_dash = User.create!(email:"rainbow_dash@email.com", password:"User@us3r")
-      binding.pry
-      # @rarity = User.create!(email:"rarity@email.com", password:"User@us3r")
-      # @applejack = User.create!(email:"applejack@email.com", password:"User@us3r")
-      # @fluttershy = User.create!(email:"fluttershy@email.com", password:"User@us3r")
+      @rainbow_dash = User.create!(name:"Rainbow Dash", email:"rainbow_dash1@email.com", password:"User@us3r")
+      # @rarity = User.create!(name:"rarity", email:"rarity@email.com", password:"User@us3r")
+      # @applejack = User.create!(name:"applejack", email:"applejack@email.com", password:"User@us3r")
+      # @fluttershy = User.create!(name:"fluttershy", email:"fluttershy@email.com", password:"User@us3r")
 
-
-
-
-      # @rainbow_dash.followers << [@rarity, @applejack, @twilight_sparkle]
-      # @twilight_sparkle.followers << [@spike, @starlight_glimmer]
-      # @pinkie_pie.followers << [@rainbow_dash, @twilight_sparkle, @rarity, @applejack, @fluttershy, @spike, @starlight_glimmer]
-
+      @twilight_sparkle.friendships.create!(friend:@spike)
+      @twilight_sparkle.friendships.create!(friend:@starlight_glimmer)
+      @rainbow_dash.friendships.create!(friend:@twilight_sparkle)
       # @party_1  = @twilight_sparkle.parties.create!(movie_id:"4", runtime:"12", date:"Oct 12, 1940", start:"12 pm")
-
-      # THIS IS WHERE WE GET STUCK
-      # - Do we need to update friendships? (if I friend you, shouldnt it automatically friend me back?)
-      # - PartyParticipants and Friendships arent working
-      # - This MAY have to be top down so that the features can drive the database
-      # - We may have to go back to the idea of an array of participant ids (this seems easier to update in the long run)
-      # binding.pry
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@twilight_sparkle)
 
@@ -51,46 +39,32 @@ RSpec.describe "Dashboard Page", type: :feature do
       click_button "Discover Movies"
       expect(current_path).to eq("/discover")
       expect(page).to have_content("Welcome #{@twilight_sparkle.name.capitalize}!")
-      # expect(page).to have_content("Discover Show Page") # NEEDS TO BE UPDATED TO A DYNAMIC WELCOME HEADER
     end
 
-    it "can see friends section is empty if no friends are added" do
+
+    it "can see friends section with friends you've added" do
       within "#friends" do
-        expect(page).to have_content("You currently have no friends")
+        expect(page).to have_content(@spike.name)
+        expect(page).to have_content(@starlight_glimmer.name)
+        expect(page).to have_content(@rainbow_dash.name)
+        expect(page).to_not have_content(@pinkie_pie.name)
       end
     end
 
-    describe "when a user has friends added" do
-      before :each do
-        @twilight_sparkle.friendships.create!(friend:@spike)
-        @twilight_sparkle.friendships.create!(friend:@starlight_glimmer)
-        @rainbow_dash.friendships.create!(friend:@twilight_sparkle)
+    it "can add a friend" do
+      within "#friends" do
+        expect(page).to_not have_content(@pinkie_pie.name)
+        fill_in :email, with: @pinkie_pie.email
+        expect(page).to have_button("Add Friend")
+        click_button "Add Friend"
+        expect(current_path).to eq('/dashboard')
+        expect(page).to have_content(@pinkie_pie.name)
       end
-
-      it "can see friends section with friends you've added" do
-        within "#friends" do
-          expect(page).to have_content(@spike.name)
-          expect(page).to have_content(@starlight_glimmer.name)
-          expect(page).to have_content(@rainbow_dash.name)
-          expect(page).to_not have_content(@pinkie_pie.name)
-        end
-      end
-
-      it "can add a friend" do
-        within "#friends" do
-          expect(page).to_not have_content(@pinkie_pie.name)
-          fill_in :email, with: @pinkie_pie.email
-          expect(page).to have_button("Add Friend")
-          click_button "Add Friend"
-          expect(current_path).to eq('/dashboard')
-          expect(page).to have_content(@pinkie_pie.name)
-        end
-      end
-
-      it "can see error if friend is not in database (I'm sorry your friend cannot be found)"
     end
+
+    it "can see error if friend is not in database (I'm sorry your friend cannot be found)"
     it "can see a parties section with pertinent info"
-      # - Invited
+    #   # - Invited
       #   - Movie Title
       #   - Date and Time
       #   - Status == Invited
@@ -107,5 +81,18 @@ RSpec.describe "Dashboard Page", type: :feature do
     end
 
     # it "can redirect you to welcome? (does this imply a custom 400 error page?)"
+  end
+
+  describe "As a registered user with no friends" do
+    before :each do
+      @no_friend_user = User.create!(name:"Friendless", email:"bemyfriend@email.com", password:"User@us3r")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@no_friend_user)
+    end
+
+    it "can see friends section is empty if no friends are added" do
+      within "#friends" do
+        expect(page).to have_content("You currently have no friends")
+      end
+    end
   end
 end
