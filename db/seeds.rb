560d2b6971d643ea8e57cd73ef56e6691ebf9542
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-14 15:11:28 -0600

    SUBJECT: Add initial seeds for possible databasing on deploy
    Co-authored-by: Arique <60521756+Arique1104@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Destroy.all
+
+# @twilight_sparkle = User.create!(name:"Twilight", email:"twilight_sparkle@email.com", password:"User@us3r")
 #
-# Examples:
+# @spike = User.create!(name:"spike", email:"spike@email.com", password:"User@us3r")
+# @starlight_glimmer = User.create!(name:"Starlight", email:"starlight_glimmer@email.com", password:"User@us3r")
+# @pinkie_pie = User.create!(name:"pinkie", email:"pinkie_pie@email.com", password:"User@us3r")
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# @rainbow_dash = User.create!(name:"Rainbow Dash", email:"rainbow_dash1@email.com", password:"User@us3r")
+# # @rarity = User.create!(name:"rarity", email:"rarity@email.com", password:"User@us3r")
+# # @applejack = User.create!(name:"applejack", email:"applejack@email.com", password:"User@us3r")
+# # @fluttershy = User.create!(name:"fluttershy", email:"fluttershy@email.com", password:"User@us3r")
+#
+# @twilight_sparkle.friendships.create!(friend:@spike)
+# @spike.friendships.create!(friend:@twilight_sparkle)
+# @twilight_sparkle.friendships.create!(friend:@starlight_glimmer)
+# @starlight_glimmer.friendships.create!(friend:@twilight_sparkle)
+# @rainbow_dash.friendships.create!(friend:@twilight_sparkle)
+# @twilight_sparkle.friendships.create!(friend:@rainbow_dash)
+# # @party_1  = @twilight_sparkle.parties.create!(movie_id:"4", runtime:"12", date:"Oct 12, 1940", start:"12 pm")
