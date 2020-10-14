1b2aad431868c0cb48fdfa74e93590357186a9ff
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-14 12:42:48 -0500

    Add a class method to movie facade self.movie_details(movie_id) which makes three API calls

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -12,4 +12,12 @@ class MovieFacade
       Movie.new(movie_data)
     end
   end
+
+  def self.movie_details(movie_id)
+    details = {}
+    details[:movie_details] = MovieService.movie_details(movie_id)
+    details[:cast_details] = MovieService.cast_details(movie_id)
+    details[:reviews] = MovieService.reviews(movie_id)
+    @movie = MovieDetails.new(details)
+  end
 end
