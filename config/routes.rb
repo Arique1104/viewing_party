a70e43b168ee7ad3dacd4757cd4b2272e28901e3
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-14 17:40:14 -0600

    SUBJECT: Add new party path
    DETAILS:
    - Updates for single quotes for rubocop
    - Add get for new party pathway with a nested movie id in uri
    - Remove extraneous comment route
    
    Co-authored-by: Arique <60521756+Arique1104@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get "/", to: 'welcome#index', as: :welcome
-  # resources :welcome, :path => '/', only: :index, as: :welcome
+  get '/', to: 'welcome#index', as: :welcome
   get '/discover', to: 'discover#show'
 
   get '/movies/top_forty', to: 'movie#top_forty'
   get '/movies/search', to: 'movie#search'
   get '/movies/:id', to: 'movie#show'
 
-  get "/register", to: "users#new"
-  post "/users", to: "users#create"
-  get "/dashboard", to: "users#show"
-  post "/dashboard", to: "sessions#create"
-  post "/friends", to: "users#add_friend"
+  get '/register', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/dashboard', to: 'users#show'
+  post '/dashboard', to: 'sessions#create'
+  post '/friends', to: 'users#add_friend'
+
+  get '/:movie_id/party/new', to: 'parties#new'
 end
