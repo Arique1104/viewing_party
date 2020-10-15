698e1a781f7817cd53e70e4a8c872238e0b29e13
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-15 06:25:55 -0600

    SUBJECT: Update movies results spec
    DETAILS:
    - Renamed search_by_movie_title_spec to index_spec
    - Left comments in areas we need more robust testing
    - Add sad path for unregistered user

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -3,10 +3,7 @@ require "rails_helper"
 RSpec.describe "Discover Landing Page" do
   describe "As an authorized user" do
     before(:each) do
-      # will add back in once we establish authentication
       @wilmer = User.create!(name:"wilmer", email: "wilmer@example.com", password: "User@us3r")
-      # # require "pry"; binding.pry
-      #
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@wilmer)
       @results = {:popularity=>1425.298,
                  :vote_count=>1762,
@@ -25,17 +22,15 @@ RSpec.describe "Discover Landing Page" do
                  :release_date=>"2020-09-23"}
     end
     it "can click on button for top 40 movies return" do
-
       visit '/discover'
-
       expect(page).to have_button("Top 40 Movies")
     end
 
     it "can return results of top forty films" do
       visit '/discover'
       click_on "Top 40 Movies"
-      expect(current_path).to eq('/movies/top_forty')
-      json_response = File.read('spec/fixtures/top_forty_results.json')
+      expect(current_path).to eq('/movies')
+      # json_response = File.read('spec/fixtures/top_forty_results.json')
       # parsed = JSON.parse(json_response, symbolize_names: true)
 
       # stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["MOVIE_API_KEY"]}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
@@ -48,8 +43,15 @@ RSpec.describe "Discover Landing Page" do
       visit '/discover'
       fill_in :query, with: "Antman"
       click_on "Search Movies"
-      expect(current_path).to eq("/movies/search")
-      expect(page).to have_content("Search Results")
+      expect(current_path).to eq("/movies")
+      # expect(page).to have_content("Search Results")
+    end
+  end
+
+  describe "As a non-registered user" do
+    it "can see 400 errors when trying to access the database" do
+      visit '/discover'
+      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
