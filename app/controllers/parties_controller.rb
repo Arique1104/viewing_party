ac4f387f8c54110b78d73e0d3d23f87aacd7aa1d
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-14 22:12:44 -0500

    SUBJECT: Code for the create action in parties controller
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -9,14 +9,15 @@ class PartiesController < ApplicationController
   def create
     date = "#{party_params["day(2i)"]}/#{party_params["day(3i)"]}/#{party_params["day(1i)"]}"
     start_time = "#{party_params["start_time(4i)"]}:#{party_params["start_time(5i)"]}"
-
-
     new_party = current_user.parties.new(movie_title: party_params[:movie_title], runtime: party_params[:runtime], date: date, start_time: start_time)
+
     if new_party.save
-require "pry"; binding.pry
+      participants = User.where(id: params[:participants])
+      participants.each do |participant|
+        new_party.party_participants.create(user: participant)
+      end
       redirect_to '/dashboard'
     else
-      require "pry"; binding.pry
       generate_flash(new_party)
       render :new
     end
