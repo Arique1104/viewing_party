8d4fbd70e99ec67890badddb0462799d68dc58b2
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-14 22:12:44 -0500

    SUBJECT: Refactor and sad pathing for create action in Party Controllers
    Co-authored-by: Priya Power 
    <49959312+priyapower@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -7,11 +7,9 @@ class PartiesController < ApplicationController
   end
 
   def create
-    date = "#{party_params["day(2i)"]}/#{party_params["day(3i)"]}/#{party_params["day(1i)"]}"
-    start_time = "#{party_params["start_time(4i)"]}:#{party_params["start_time(5i)"]}"
-    new_party = current_user.parties.new(movie_title: party_params[:movie_title], runtime: party_params[:runtime], date: date, start_time: start_time)
-
-    if new_party.save
+    schedule = Schedule.new(party_params)
+    new_party = current_user.parties.new(movie_title: party_params[:movie_title], runtime: party_params[:runtime], date: schedule.date, start_time: schedule.start_time)
+    if new_party.save && params[:participants]
       participants = User.where(id: params[:participants])
       participants.each do |participant|
         new_party.party_participants.create(user: participant)
@@ -19,13 +17,13 @@ class PartiesController < ApplicationController
       redirect_to '/dashboard'
     else
       generate_flash(new_party)
-      render :new
+      '/party'
     end
   end
 
   private
 
   def party_params
-    params.permit(:movie_title, :runtime, "day(1i)", "day(2i)", "day(3i)", "start_time(4i)", "start_time(5i)")
+    params.permit(:movie_title, :runtime, 'day(1i)', 'day(2i)', 'day(3i)', 'start_time(4i)', 'start_time(5i)')
   end
 end
